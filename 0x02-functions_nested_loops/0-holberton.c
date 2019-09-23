d60ba42dc0c806967f4d12c592a27bd6f68cdd64
dagute <1271@holbertonschool.com>
2019-09-23 22:19:54 +0000

    Putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * main - Prints holberton
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char x[] = "holberton";
+	int j;
+
+	for (j = 0; j < 9; j++)
+	{
+		_putchar(x[j]);
+	}
+	_putchar('\n');
+	return (0);
+}
