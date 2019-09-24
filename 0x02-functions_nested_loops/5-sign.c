1ed53ea00f83d20ada201d1de4728cfd10a2ea45
dagute <1271@holbertonschool.com>
2019-09-24 15:09:20 +0000

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * print_sign - prints the sign of a number
+ *
+ * @n: Number to be checked
+ *
+ * Return: 1 if n is positive, 0 if n is zero, -1 if n is negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar ('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else
+	{
+		_putchar ('-');
+		return ('/');
+	}
+}
