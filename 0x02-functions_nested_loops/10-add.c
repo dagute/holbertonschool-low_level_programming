a28418a90253c9eef2f02cb203c0c829f722436f
dagute <1271@holbertonschool.com>
2019-09-24 16:07:34 +0000

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * add - Adds two integers
+ *
+ * @a: The first number to be add
+ * @b: The other number to be add
+ *
+ * Return: The result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
