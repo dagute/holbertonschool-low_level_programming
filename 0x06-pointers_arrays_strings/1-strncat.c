2dc8dee6d4436a35013e8d46344ff4a6ea58ff03
dagute <1271@holbertonschool.com>
2019-10-04 01:36:38 +0000

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strncat: concatenates two strings
+ * @dest: string to copy
+ * @src: string
+ * @n: bytes from src to append
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x = 0;
+	int y = 0;
+
+	while (dest[y] != '\0')
+		y++;
+
+	while (x < n && src[x] != '\0')
+	{
+		dest[y + x] = src[x];
+		x++;
+	}
+
+	return (dest);
+}
