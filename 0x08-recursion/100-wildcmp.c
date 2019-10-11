c5219ddba44f7892602f2dbd6ba5248039aa13fc
dagute <1271@holbertonschool.com>
2019-10-11 03:45:14 +0000

    Compare two strings

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * wildcmp - compares two strings
+ * @s1: first input string
+ * @s2: second input string
+ * Return: 1 if string is identical, otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*' && (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1)))
+		return (1);
+	if (*(s1 + 1) != '\0' && *s2 == '\0' && *s2 == '*')
+		return (0);
+	else
+		return (0);
+}
