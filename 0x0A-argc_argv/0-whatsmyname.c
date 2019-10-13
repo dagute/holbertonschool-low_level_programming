0bf9e5e0ad55005287332b27429a4783db9a5515
dagute <1271@holbertonschool.com>
2019-10-13 21:23:04 +0000

    my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if succes
+ */
+int main (int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
