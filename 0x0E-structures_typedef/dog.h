57e44dfc6b4cd6b1fb4ef18a581d13372ef1320a
dagute <1271@holbertonschool.com>
2019-10-21 20:45:13 +0000

    Django

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,15 +5,15 @@
  * @name: first member
  * @age: second member
  * @owner: third member
+ *
  * Description: new type struct dog
  */
-
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
