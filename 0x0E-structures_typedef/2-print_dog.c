3f1871e0a2e55858cadc39bcd94890989b92578e
dagute <1271@holbertonschool.com>
2019-10-21 22:13:11 +0000

    A dog is inconditional love

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,13 +11,13 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if (d->name)
-			printf("Name:%s\n", d->name);
+			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
-		printf("Age:%f\n", d->age);
+		printf("Age: %f\n", d->age);
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("(nil)\n");
+			printf("Owner: (nil)\n");
 	}
 }
