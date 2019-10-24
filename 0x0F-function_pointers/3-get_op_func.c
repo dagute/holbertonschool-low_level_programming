1a76dce232bcaf504ab2a9ebec4afa78117fdb53
dagute <1271@holbertonschool.com>
2019-10-24 05:00:48 +0000

    select function

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * get_op_func - selects the correct function to perform the operation asked
+ * @s: operator passed as argument
+ * Return: an integer value
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	i = 0;
+
+	while (i < 5)
+	{
+
+		if (ops[i].op[0] == s[0] && s[1] == '\0')
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+
+}
