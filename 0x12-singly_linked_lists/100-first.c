7cd74ea9e69278ec6afc24aeebee9a90ebb1533f
dagute <1271@holbertonschool.com>
2019-11-01 06:24:21 +0000

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * beforethemain - finction that it is executed before the main
+ * Return: nothing
+ */
+void beforethemain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
