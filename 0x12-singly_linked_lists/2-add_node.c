aaed8ecf2c9262e296a579053b448ade37538ed2
dagute <1271@holbertonschool.com>
2019-11-01 04:49:38 +0000

    adds new node to the list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * _strlen - length of the string
+ * @s: string
+ * Return: length of the string
+ */
+int _strlen(const char *s)
+{
+	unsigned int x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+	return (x);
+}
+/**
+ * add_node - adds a node
+ * @head: pointer to the new list
+ * @str: string
+ * Return: address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newone;
+
+	if (head == NULL)
+		return (NULL);
+
+	newone = malloc(sizeof(list_t));
+	if (newone == NULL)
+		return (NULL);
+
+	newone->str = strdup(str);
+	newone->len = _strlen(str);
+	newone->next = *head;
+
+	*head = newone;
+	return (*head);
+}
