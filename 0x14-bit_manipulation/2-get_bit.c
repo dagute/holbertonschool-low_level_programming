878398c051ecfc237bdb9ab6e5fbeae88f7bd3b0
dagute <1271@holbertonschool.com>
2019-11-08 00:28:02 +0000

    value of a bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -7,6 +7,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index > sizeof(n) * 8 - 1)
+	{
+		return (-1);
+	}
 	if ((n >> index) & 1)
 	{
 		return (1);
@@ -15,8 +19,5 @@ int get_bit(unsigned long int n, unsigned int index)
 	{
 		return (0);
 	}
-	if (index > (sizeof(n) * 8) - 1)
-	{
-		return (-1);
-	}
+
 }
