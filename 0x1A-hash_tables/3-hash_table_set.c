6fbaef1c6453e9141c640ea8bf20406839285aae
dagute <1271@holbertonschool.com>
2020-01-18 23:53:02 +0000

    adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,25 +11,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node, *newone;
 	int index;
 
-	if (!value || !key || !!ht)
+	if (!ht || !key || !value)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
-	if (ht->array[index])
+	node = ht->array[index];
+	while (node != NULL)
 	{
-		node = ht->array[index];
-		while (node != NULL)
+		if (strcmp(node->key, key) == 0)
 		{
-			if (strcmp(node->key, key) == 0)
-			{
-				free(node->value);
-				node->value = strdup(value);
-				if (node->value == NULL)
-					return (0);
-				else
-					return (1);
-			}
-			node = node->next;
+			free(node->value);
+			node->value = strdup(value);
+			if (node->value == NULL)
+				return (0);
+			return (1);
 		}
+		node = node->next;
 	}
 	newone = malloc(sizeof(hash_node_t));
 	if (newone == NULL)
